4656ff7caa68baccf7a7015af6bf5c969fff73be
Jhonatas Antonelli <jonatasantonelli@gmail.com>
2022-03-13 15:50:30 -0300

    resolvido problema dos caracteres

--- Ex111/utilidade/dado/__init__.py
+++ Ex111/utilidade/dado/__init__.py
@@ -1,9 +1,10 @@
 def leiapreço (msg):
     valido = False
     while not valido:
-        entrada = str(input(msg)).replace(',', '.')
-        if entrada.isalpha() or entrada.strip() == '':
-            print(f'ERRO: \"{entrada}\" não é um preço invalido!')
-        else:
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isnumeric():
             valido = True
             return float(entrada)
+        else:
+            print(f'ERRO: \"{entrada}\" não é um preço invalido!')
+
