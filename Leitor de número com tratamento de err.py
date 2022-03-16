106f916ba34eeb76a51e0c6c8b529029bb81136c
Jhonatas Antonelli <jonatasantonelli@gmail.com>
2022-03-16 15:53:36 -0300

    ok

--- Leitor de número com tratamento de err.py
+++ Leitor de número com tratamento de err.py
@@ -1,37 +1,17 @@
-def leiaint(msg):
-    try:
-        n = int(input('Digite um número inteiro: '))
-    except(ValueError, TypeError):
-        print('Tivemos um problema com os tipos de dados que você digitou')
-        n = 'ERRO'
-        return n
-    except KeyboardInterrupt:
-        print('O usuário preferiu não informar os valores')
-        n = 'ERRO'
-        return n
-    except Exception as erro:
-        print(f'O erro encontrado foi {erro.__cause__}')
-        n = 'ERRO'
-        return n
-    else:
-        return n
-def leiafloat(msg):
-    try:
-        f = float(input('Digite um número real: '))
-    except(ValueError, TypeError):
-        f = 'ERRO'
-        print('Tivemos um problema com os tipos de dados que você digitou')
-        return f
-    except KeyboardInterrupt:
-        print('O usuário preferiu não informar os valores')
-        f = 'ERRO'
-        return f
-    except Exception as erro:
-        print(f'O erro encontrado foi {erro.__cause__}')
-        f = 'ERRO'
-        return f
-    else:
-        return f
-n = leiaint('Digite um número inteiro')
-f = leiafloat('Digite um número real')
-print(f'Os números que você digitou foram {n} e {f}')
+def validacao(x, msg, tipo):
+    while True:
+        try:
+            tipo (x)
+        except(ValueError, TypeError):
+            print('Tivemos um problema com os tipos de dados que você digitou')
+        except KeyboardInterrupt:
+            print('O usuário preferiu não informar os valores')
+        except Exception as erro:
+            print(f'O erro encontrado foi {erro.__cause__}')
+        else:
+            return x
+            break
+        x = input(f'Digite um número {msg}: ').replace(',', '.').strip()
+n = validacao((input('Digite um número inteiro: ')), 'inteiro', int).strip()
+r = validacao((input('Digite um número real: ')).replace(',', '.').strip(), 'real', float)
+print(f'Você digitou os números {n} e {r}')
